d957f3bf7df3cf213f2148c145e82d409f42767c
Thomas Børstad <4872288+tboerstad@users.noreply.github.com>
2021-12-13 00:07:23 +0100

    Day 12

--- 12/12.py
+++ 12/12.py
@@ -1,7 +1,9 @@
 from collections import defaultdict
+from functools import cache
 
 neighbours = defaultdict(set)
 
+@cache
 def small_cave_visited_twice(current_path):
     for idx, current_node in enumerate(current_path):
         if current_node.islower() and current_node in current_path[idx+1:]:
@@ -13,21 +15,27 @@ for line in open("input.txt").read().splitlines():
     neighbours[a].add(b)
     neighbours[b].add(a)
 
-def count(cave='start', seen=[]):
+@cache
+def count(cave='start', seen=tuple()):
     if cave == 'end': return 1
     if cave in seen:
         if cave.islower(): return 0
+    if cave.islower():
+        seen = (*seen,cave)
     
-    return sum(count(n, seen+[cave]) for n in neighbours[cave])
+    return sum(count(n, seen) for n in neighbours[cave])
 
-def count2(cave='start', seen=[]):
+@cache
+def count2(cave='start', seen=tuple()):
     if cave == 'end': return 1
     if cave in seen:
         if cave.islower():
             if cave == 'start': return 0
             if small_cave_visited_twice(seen): return 0
+    if cave.islower():
+        seen = (*seen,cave)
     
-    return sum(count2(n, seen+[cave]) for n in neighbours[cave])
+    return sum(count2(n, seen) for n in neighbours[cave])
 
 print(count())
 print(count2())
