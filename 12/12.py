9c114732e74ed81e60a79bb037415e689ee885e3
Thomas Børstad <4872288+tboerstad@users.noreply.github.com>
2021-12-12 23:56:01 +0100

    Day 12

--- 12/12.py
+++ 12/12.py
@@ -1,40 +1,33 @@
 from collections import defaultdict
 
-caves = set(["start","end"])
 neighbours = defaultdict(set)
-
-def big_cave(cave):
-    return cave.upper() == cave
-
-def can_visit_p1(cave, current_path):
-    return big_cave(cave) or cave not in current_path
 
 def small_cave_visited_twice(current_path):
     for idx, current_node in enumerate(current_path):
-        if not big_cave(current_node):
-            if current_node in current_path[idx+1:]:
+        if current_node.islower() and current_node in current_path[idx+1:]:
                 return True
     return False
 
-def can_visit_p2(cave, current_path):
-    if cave in ['start', 'finish']:
-        return False
-    if small_cave_visited_twice(tuple(current_path)):
-        return can_visit_p1(cave, current_path)
-    return big_cave(cave) or current_path.count(cave) < 2
-
-def collect_paths(current_path, pred):
-    if current_path[-1] == 'end':
-        return 1
-    caves_to_visit = [x for x in neighbours[current_path[-1]] if pred(x,tuple(current_path))]
-    return sum([collect_paths(current_path+[n], pred) for n in caves_to_visit])
-
 for line in open("input.txt").read().splitlines():
     a, b = line.split('-')
-    caves.add(a)
-    caves.add(b)
     neighbours[a].add(b)
     neighbours[b].add(a)
 
-print(collect_paths(['start'],can_visit_p1))
-print(collect_paths(['start'],can_visit_p2))
+def count(cave='start', seen=[]):
+    if cave == 'end': return 1
+    if cave in seen:
+        if cave.islower(): return 0
+    
+    return sum(count(n, seen+[cave]) for n in neighbours[cave])
+
+def count2(cave='start', seen=[]):
+    if cave == 'end': return 1
+    if cave in seen:
+        if cave.islower():
+            if cave == 'start': return 0
+            if small_cave_visited_twice(seen): return 0
+    
+    return sum(count2(n, seen+[cave]) for n in neighbours[cave])
+
+print(count())
+print(count2())
