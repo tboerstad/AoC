d7ea6a52ee4e153af8faf6b3922027d3e895ce4e
Thomas Børstad <4872288+tboerstad@users.noreply.github.com>
2022-12-15 12:36:06 +0100

    Day 14

--- 2022/14/14.zig
+++ 2022/14/14.zig
@@ -1,16 +1,10 @@
 const std = @import("std");
 const print = std.debug.print;
 
+const M = 1000;
+const N = 1000;
 
-// const M = 169;
-// const N = 85;
-
-const M = 10;
-const N = 10;
-
-var map: [M][N]bool = [_][N]bool { [_]bool{false} ** N } ** M;
-
-
+var map: [M][N]bool = [_][N]bool{[_]bool{false} ** N} ** M;
 
 const Point = struct {
     const Self = @This();
@@ -21,79 +15,94 @@ const Point = struct {
         var coords = std.mem.tokenize(u8, s, ",");
         var x = try std.fmt.parseInt(u32, coords.next().?, 10);
         var y = try std.fmt.parseInt(u32, coords.next().?, 10);
-        return Self{ .x=x, .y=y };
+        return Self{ .x = x, .y = y };
     }
-    fn init_normalized(s: []const u8, x_min: u32) !Self {
-        var p = try Self.init(s);
-        return Self{ .x=p.x-x_min, .y=p.y };
-    }     
 };
-
-fn solve(input: []const u8) !void {
-    // var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-    // defer arena.deinit();
-    // const alloc = arena.allocator();
-
-    var iter = std.mem.split(u8, input, "\n");
-    var x_max: u32 = 0;
-    var y_max: u32 = 0;
-    var x_min: u32 = 9999999;
-    while (iter.next()) |line| {
-        var coords = std.mem.tokenize(u8, line, " -> ");
-        while (coords.next()) |xy| {
-            var p = try Point.init(xy);
-            x_max = @max(x_max, p.x);
-            x_min = @min(x_min, p.x);
-            y_max = @max(y_max, p.y);
-        }
-    }
-
-    iter = std.mem.split(u8, input, "\n");
-    while (iter.next()) |line| {
-        var coords = std.mem.tokenize(u8, line, " -> ");
-        var start = try Point.init_normalized(coords.next().?, x_min);
-        while (coords.next()) |xy| {
-            var end = try Point.init_normalized(xy, x_min);
-
-            map[start.x][start.y] = true;
-            while (start.x != end.x and start.y != end.y) {
-                print( "{any}\n", .{start});
-                print( "{any}\n", .{end});
-                if (end.x != start.x) { start.x = if(start.x < end.x) start.x+1 else start.x-1; }
-                if (end.y != start.y) { start.y = if(start.y < end.y) start.y+1 else start.y-1; }
-                map[start.x][start.y] = true;
-            }
-        }
-    }
-
-    for (map) |row| {
+fn printMap(m: [M][N]bool) void {
+    print("\n", .{});
+    for (m) |row| {
         print("\n", .{});
         for (row) |c| {
             if(c) {
-                print(".", .{});
+                print("#", .{});
             } else {
-                print("*", .{});
+                print(".", .{});
+            }
+        }
+    }
+}
+fn solve(input: []const u8) !void {
+    for (map) |*row| {
+        for (row) |*c| {
+            c.* = false;
+        }
+    }
+    var y_max: u32 = 0;
+
+    var iter = std.mem.split(u8, input, "\n");
+    while (iter.next()) |line| {
+        var coords = std.mem.tokenize(u8, line, " -> ");
+        var start = try Point.init(coords.next().?);
+        y_max = @max(y_max, start.y);
+        while (coords.next()) |xy| {
+            var end = try Point.init(xy);
+            y_max = @max(y_max, end.y);
+            map[start.y][start.x] = true;
+            while (start.x != end.x or start.y != end.y) {
+                if (end.x != start.x) {
+                    start.x = if (start.x < end.x) start.x + 1 else start.x - 1;
+                }
+                if (end.y != start.y) {
+                    start.y = if (start.y < end.y) start.y + 1 else start.y - 1;
+                }
+                map[start.y][start.x] = true;
             }
         }
     }
 
-    // print("\n{any}\n", .{map});
-    print("\nx: ({d},{d}), y-max: (0,{d})\n", .{x_min, x_max, y_max});
+    for (map[y_max+2]) |*c| { c.* =true; }
+
+    var cnt: usize = 0;
+    var p1: usize = std.math.maxInt(usize);
+    var p2: usize = 0;
+    outer: while (true) {
+        var sand = try Point.init("500,0");
+        while (true) {
+            if (map[sand.y + 1][sand.x] == false) {
+                sand.y += 1;
+            } else if (map[sand.y + 1][sand.x - 1] == false) {
+                sand.y += 1;
+                sand.x -= 1;
+            } else if (map[sand.y + 1][sand.x + 1] == false) {
+                sand.y += 1;
+                sand.x += 1;
+            } else {
+                map[sand.y][sand.x] = true;
+                if (sand.y == 0) {
+                    p2 = cnt+1;
+                    break :outer;
+                }
+            }
+
+            if (sand.y == y_max) {
+                p1 = @min(p1,cnt);
+            }
+            if (sand.y > y_max+2) {
+                break;
+            }
+        }
+        cnt += 1;
+    }
+
+    print("\nP1: {d}\n", .{p1});
+    print("P2: {d}\n", .{p2});
 
 }
 
-// test "input 1" {
-//     try solve(@embedFile("input.txt"));
-// }
-
-// test "input 2" {
-//     try p2(@embedFile("input.txt"));
-// }
+test "input 1" {
+    try solve(@embedFile("input.txt"));
+}
 
 test "ex 1" {
     try solve(@embedFile("ex.txt"));
 }
-
-// test "ex 2" {
-//     try p2(@embedFile("ex.txt"));
-// }
