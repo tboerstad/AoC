169dc82ec880750e958852b5d398a742bc5cfa62
Thomas Børstad <4872288+tboerstad@users.noreply.github.com>
2023-12-06 21:52:58 +0100

    Day 06

---
+++ 2023/06/06.hs
@@ -0,0 +1,25 @@
+import Data.List.Split (splitOn)
+
+parseProblem :: String -> ([Int], [Int])
+parseProblem input = case splitOn "\n" input of
+    (x : y : _) -> (parseLine x, parseLine y)
+    where
+        parseLine = map read . tail . words
+
+computeAllSolutions :: Int -> Int -> [Int]
+computeAllSolutions 0 _ = [0]
+computeAllSolutions time speed =  time*speed : computeAllSolutions (time-1) (speed+1)
+
+part1 :: ([Int], [Int]) -> Int
+part1 (times, dists) = product $ map length $ zipWith (\time dist -> filter (>dist) (computeAllSolutions time 0)) times dists
+
+fixKerning :: [Int] -> Int
+fixKerning = read . concatMap show
+
+main :: IO ()
+main = do
+    content <- readFile "input.txt"
+    let (times, dists) = parseProblem content
+    print $ part1 (times, dists)
+    print $ part1 ([fixKerning times],[fixKerning dists])
+
